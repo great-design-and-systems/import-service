97c71725fdffcedf829ffc37d23f2bee192e7eb0
rickzx98 <rickzx98@gmail.com>
2016-08-19 02:11:13 +0800

    updates

--- src/control/get-import-logs.js
+++ src/control/get-import-logs.js
@@ -3,7 +3,7 @@ var ImportLog = require('../entity/import-log');
 
 function execute(importId, callback) {
     ImportLog.find({
-        importId: 'importId'
+        importId: importId
     }, function(err, result) {
         if (err) {
             console.error('get-import-logs', err);
